c8582dd772d20402d2f40ff3eb7cbd3ef15013c0
Denis K <kalantaevskii@gmail.com>
2025-08-23 23:38:24 +0200

    add constant folding for boolean functions inside MultiIfConstFolding
    Now it's able to collapse the following request:
    ```
    SELECT multiIf(atype = 1, IPv4NumToString(reinterpretAsUInt32(reverse(s))), atype = 28, IPv6NumToString(toFixedString(s, 16)), s) FROM (SELECT 99 as atype, 'abcdefghijklmnopq' as s);
    ```

---
+++ tests/queries/0_stateless/03593_multiif_constant_condition_folding.sql
@@ -0,0 +1,20 @@
+SELECT multiIf(atype = 1, IPv4NumToString(reinterpretAsUInt32(reverse(s))), atype = 28, IPv6NumToString(toFixedString(s, 16)), s)
+FROM
+(
+    SELECT
+        99 AS atype,
+        'abcdefghijklmnopq' AS s
+);
+
+WITH test AS
+    (
+        SELECT 0 AS number
+    )
+SELECT if(number = 0, 0, intDiv(42, number))
+FROM test;
+
+SELECT if(1 = 1, 1, toInt8('x'));
+
+SELECT multiIf(1 = 1, 1, 1 = 2, 2, toInt8('x'));
+
+SELECT CASE WHEN 1=1 THEN 1 ELSE toInt8('x') END;
